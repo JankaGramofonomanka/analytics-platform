0b9f6ccb07f4d5da190aa80acca0cc588eb488ad
JankaGramofonomanka <tsrn666@gmail.com>
2022-07-26 11:35:40 +0200

    Delete unused code
    Deleted "Hello World" and "Joke" functionality that came with the template.

--- src/main/scala/io/github/JankaGramofonomanka/analyticsplatform/AnalyticsplatformServer.scala
+++ src/main/scala/io/github/JankaGramofonomanka/analyticsplatform/AnalyticsplatformServer.scala
@@ -4,7 +4,6 @@ import cats.effect.{Async, Resource}
 import cats.syntax.all._
 import com.comcast.ip4s._
 import fs2.Stream
-import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits._
 import org.http4s.server.middleware.Logger
@@ -16,26 +15,20 @@ import io.github.JankaGramofonomanka.analyticsplatform.codecs.JsonCodec
 object AnalyticsplatformServer {
 
   def stream[F[_]: Async](db: KeyValueDB[F], topic: TagTopic[F], codec: JsonCodec[F]): Stream[F, Nothing] = {
+    val ops = new KVFrontend[F](db, topic)
+
+    // Combine Service Routes into an HttpApp.
+    // Can also be done via a Router if you
+    // want to extract segments not checked
+    // in the underlying routes.
+    val httpApp = (
+      AnalyticsplatformRoutes.kvRoutes(ops, codec)
+    ).orNotFound
+
+    // With Middlewares in place
+    val finalHttpApp = Logger.httpApp(true, true)(httpApp)
+    
     for {
-      client <- Stream.resource(EmberClientBuilder.default[F].build)
-      helloWorldAlg = HelloWorld.impl[F]
-      jokeAlg = Jokes.impl[F](client)
-
-      ops = new KVFrontend[F](db, topic)
-
-      // Combine Service Routes into an HttpApp.
-      // Can also be done via a Router if you
-      // want to extract segments not checked
-      // in the underlying routes.
-      httpApp = (
-        AnalyticsplatformRoutes.helloWorldRoutes[F](helloWorldAlg) <+>
-        AnalyticsplatformRoutes.jokeRoutes[F](jokeAlg) <+>
-        AnalyticsplatformRoutes.kvRoutes(ops, codec)
-      ).orNotFound
-
-      // With Middlewares in place
-      finalHttpApp = Logger.httpApp(true, true)(httpApp)
-
       exitCode <- Stream.resource(
         EmberServerBuilder.default[F]
           .withHost(ipv4"0.0.0.0")
