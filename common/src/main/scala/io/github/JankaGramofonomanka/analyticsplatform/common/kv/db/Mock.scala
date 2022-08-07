5922554be9cfc030f4b2bed5c4a1f6945f3aef4b
JankaGramofonomanka <tsrn666@gmail.com>
2022-08-07 17:52:48 +0200

    Get rid of `...common.kv.db.Mock.Aggregator`

--- common/src/main/scala/io/github/JankaGramofonomanka/analyticsplatform/common/kv/db/Mock.scala
+++ common/src/main/scala/io/github/JankaGramofonomanka/analyticsplatform/common/kv/db/Mock.scala
@@ -8,7 +8,6 @@ import cats.implicits._
 import io.github.JankaGramofonomanka.analyticsplatform.common.Data._
 import io.github.JankaGramofonomanka.analyticsplatform.common.Utils
 import io.github.JankaGramofonomanka.analyticsplatform.common.kv.db.{ProfilesDB, AggregatesDB}
-import io.github.JankaGramofonomanka.analyticsplatform.common.kv.topic.Topic
 
 
 class Mock[F[_]: Monad](
@@ -31,29 +30,6 @@ class Mock[F[_]: Monad](
   def updateAggregate(key: AggregateKey, value: AggregateValue): F[Unit]
     = pure(aggregates.put(key, value)) >> pure(())
 
-
-
-  object Aggregator extends Topic.Publisher[F, UserTag] {
-
-    private def getAggregate(key: AggregateKey): F[AggregateValue]
-      = pure(aggregates.get(key).getOrElse(AggregateValue.default))
-    
-    private def getAggregateKV(key: AggregateKey): F[(AggregateKey, AggregateValue)] = for {
-      v <- getAggregate(key)
-    } yield (key -> v)
-
-    def publish(tag: UserTag): F[Unit] = {
-      val keys = AggregateKey.fromTag(tag)
-      for {
-        toUpdate <- keys.traverse(key => getAggregateKV(key))
-        toAdd = toUpdate.map { case (k, v)
-          => (k -> AggregateValue(v.count + 1, v.sumPrice + tag.productInfo.price))
-        }
-
-        _ <- pure(aggregates ++= toAdd)
-      } yield ()
-    }
-  }
 }
 
 object Mock {
